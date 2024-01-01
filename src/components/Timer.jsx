bdeb408b740d7ce27e2937cd14d345073117af6f
Eder Henrique <ederhscc@gmail.com>
2024-01-01 18:54:56 -0300

    added formatTime function in Timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import TimerDisplay from "./TimerDisplay";
 import TimerControls from "./TimerControls";
 import LapList from "./LapList";
@@ -6,9 +6,23 @@ import LapList from "./LapList";
 import "./Timer.css";
 
 const Timer = () => {
+  const [milliseconds, setMilliseconds] = useState(0);
+  const [timerOn, setTimerOn] = useState(false);
+  const [laps, setLaps] = useState([]);
+
+  const formatTime = () => {
+    const minutes = ("0" + (Math.floor(milliseconds / 60000) % 60)).slice(-2);
+    const seconds = ("0" + (Math.floor(milliseconds / 1000) % 60)).slice(-2);
+    const centiseconds = ("0" + (Math.floor(milliseconds / 10) % 100)).slice(
+      -2
+    );
+
+    return `${minutes}:${seconds}:${centiseconds}`;
+  };
+
   return (
     <div className="timer-container">
-      <TimerDisplay />
+      <TimerDisplay timer={formatTime()} />
       <TimerControls />
       <LapList />
     </div>
--- src/components/TimerDisplay.jsx
+++ src/components/TimerDisplay.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const TimerDisplay = () => {
-  return <div className="timer-display">00:00</div>;
+const TimerDisplay = ({ timer }) => {
+  return <div className="timer-display">{timer}</div>;
 };
 
 export default TimerDisplay;
